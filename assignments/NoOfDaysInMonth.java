e57f20f048ccb6dc751f7b703d74f081d0a9ec78
DhanaBhore <dhanashreebhore92@gmail.com>
2025-05-19 17:18:33 +0530

    New code

---
+++ assignments/NoOfDaysInMonth.java
@@ -0,0 +1,68 @@
+package assignments;
+
+import java.util.Scanner;
+
+public class NoOfDaysInMonth {
+    public static void main(String[] args) {
+
+        System.out.println("Input Month Number : ");
+
+        Scanner scan = new Scanner(System.in);
+
+        //String MonthName = "";
+        int MonthNumber = scan.nextInt();
+
+
+        int NoOfDaysInMonth = 0;
+        switch (MonthNumber){
+            case 1:
+            NoOfDaysInMonth = 31;
+            break;
+
+            case 2:
+            NoOfDaysInMonth = 28;
+            break;
+
+            case 3:
+            NoOfDaysInMonth = 31;
+            break;
+
+            case 4:
+            NoOfDaysInMonth = 30;
+            break;
+
+            case 5:
+            NoOfDaysInMonth = 31;
+            break;
+
+            case 6:
+            NoOfDaysInMonth = 30;
+            break;
+
+            case 7:
+            NoOfDaysInMonth = 31;
+            break;
+
+            case 8:
+            NoOfDaysInMonth = 31;
+            break;
+
+            case 9:
+            NoOfDaysInMonth = 30;
+            break;
+
+            case 10:
+            NoOfDaysInMonth = 31;
+            break;
+
+            case 11:
+            NoOfDaysInMonth = 30;
+            break;
+
+            case 12:
+            NoOfDaysInMonth = 31;
+            break;
+        }
+        System.out.println(NoOfDaysInMonth);
+    }
+}
