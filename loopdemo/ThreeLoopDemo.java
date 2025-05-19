dda0a35c34caebb69c9cbf644a89e76a35721972
DhanaBhore <dhanashreebhore92@gmail.com>
2025-05-19 18:14:49 +0530

    three loops

---
+++ loopdemo/ThreeLoopDemo.java
@@ -0,0 +1,32 @@
+package loopdemo;
+
+public class ThreeLoopDemo {
+
+    public static void main(String[] args) {
+       int i = 1;
+        while (i <= 10) {
+            System.out.println("Hello " + i);
+            i++;
+        }
+        System.out.println("I am out of loo");
+    }
+}
+
+
+/*class DoWhileLoopDemo {
+    int i = 20;
+    do{
+        System.out.println("Hello " + i);
+        i++;
+    }while(i<=10);
+
+        System.out("I am out of loop");
+}*/
+
+class ForLoopDemo{
+       for(int i = 0; i<=10; i++)
+
+    {
+        System.out.println("Hello " + i);
+    }
+        }
