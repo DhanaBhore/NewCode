b70a93a05951149360691b4a93cb0fd34309e607
DhanaBhore <dhanashreebhore92@gmail.com>
2025-05-20 18:27:08 +0530

    for loop

---
+++ assignments/ForLoopInTwoWays.java
@@ -0,0 +1,17 @@
+package assignments;
+
+public class ForLoopInTwoWays {
+    public static void main(String[] args) {
+        for(int i = 0; i<0; i++){
+            System.out.println(i);
+        }
+    }
+}
+
+
+/* Second way of writing for loop:
+
+    for(dataType element : arrayorCollection){
+
+    }
+*/
