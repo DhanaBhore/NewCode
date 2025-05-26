4924803cc02447908ea774a4b51c31780faf5370
DhanaBhore <dhanashreebhore92@gmail.com>
2025-05-26 15:27:00 +0530

    Object class

---
+++ assignments/AssignmentObjectClass.java
@@ -0,0 +1,188 @@
+package assignments;
+
+class Human {
+
+    String Name;
+    int Height;
+    int Weight;
+
+    void Walk(){
+
+    }
+    void talk(String words){
+
+    }
+}
+
+class Mobilephone{
+
+    int height;
+    int width;
+    String brand_name;
+    int storage;
+    String color;
+    int no_of_SIM_cards;
+    int no_of_cameras;
+    String model;
+
+
+    void calling(String receiver){
+
+    }
+
+    void messaging(String receiver){
+
+    }
+
+    void   transactions(int rupees){
+
+    }
+
+    void  shopping(String itemname){
+
+    }
+
+    void   capture_photos(){
+
+    }
+
+    void navigation(String name_of_place){
+
+    }
+
+    void entertainment(){
+
+    }
+}
+
+class Desktop {
+
+    int Display_size;
+    int height;
+    int width;
+    float weight;
+    int memory_storage;
+    String brand_name;
+    String model;
+
+
+    void entertainment() {
+    }
+
+    void work() {
+
+    }
+}
+
+class COllege{
+    String Name;
+    String Career_branches;
+    int no_of_professors;
+    String professor_Names;
+    int noOfSportsPlayed;
+
+    void learn(String SubjectName){
+
+    }
+    void play( String sport){
+
+    }
+}
+
+
+class TestSuite{
+    int NoOfTestScenarios;
+    int NoOfTestCases;
+    String TypeOfTestScript;
+
+    void run(int NoOfTestScenarios){
+
+    }
+}
+
+
+class TestCase{
+
+    int noOfSteps;
+
+    void run(int noOfSteps){
+
+    }
+}
+
+
+class TestStep{
+
+    int noOfSteps;
+
+    void run (int noOfSteps){
+
+    }
+}
+
+
+class Bike {
+    String color;
+    String manufacturer;
+    String model;
+    int mileage;
+
+    void riding(){
+
+    }
+}
+
+
+class Refridgerator {
+    int volume;
+    int no_of_doors;
+    String brand;
+    String model;
+
+    void Freeze(String items){
+
+    }
+    void keepthings(String items){
+
+    }
+}
+
+class TrolleyBag {
+    String color;
+    int size;
+    int no_of_chains;
+
+    void carry_luggage(){
+
+    }
+    void storage(String iten_name){
+
+    }
+}
+
+class Mirror {
+    int height;
+    int width;
+    String shape;
+
+    void image(){
+
+    }
+    void showcase(){
+
+    }
+}
+
+
+class GasStove {
+    float height;
+    int width;
+    int number_of_burners;
+    String name_of_company;
+
+    void cooking(String food){
+
+    }
+
+
+}
